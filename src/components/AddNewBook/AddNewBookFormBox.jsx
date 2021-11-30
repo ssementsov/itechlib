e60dc891a0d1ed31f1ec1dc8b0d240ae858190e9
LubavaZ <lubavazigalko@gmail.com>
2021-11-30 09:30:23 +0300

    delete unnecessary code

--- src/components/AddNewBook/AddNewBookFormBox.jsx
+++ src/components/AddNewBook/AddNewBookFormBox.jsx
@@ -5,18 +5,9 @@ import { Box, Container } from '@mui/material'
 import { CloseIcon } from '../../icons/close-icon'
 import AddNewBookForm from './AddNewBookForm'
 import { status } from '../../common/constants/status-constants'
-import { useSnackbar } from 'notistack'
 
 const AddNewBookFormBox = ({ handleClose }) => {
   const router = useRouter()
-  const { enqueueSnackbar } = useSnackbar()
-
-  const handleClick = () => {
-    enqueueSnackbar('Your book has been added successfully!', {
-      variant: 'success',
-      autoHideDuration: 5000,
-    })
-  }
 
   function validate(value) {
     let error = {}
@@ -74,7 +65,6 @@ const AddNewBookFormBox = ({ handleClose }) => {
     }),
     validate,
     onSubmit: () => {
-      handleClick()
       handleClose()
       router.push('/home')
     },
