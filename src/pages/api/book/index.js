f76c9cbe599f89adcaef6f772c88689d45cb4a93
LubavaZ <lubavazigalko@gmail.com>
2021-12-10 16:54:17 +0300

    add json-file

--- src/pages/api/book/index.js
+++ src/pages/api/book/index.js
@@ -1,49 +1,15 @@
-export let bookList = [
-  {
-    id: 1,
-    title: "Book 1",
-    author: "Ivan Ivanov",
-    category: "Technical",
-    language: "English",
-    link: "https://www.amazon.com/C-Programming-Language-4th/dp/0321563840",
-    rating: 0,
-    status: "Available",
-    description: "Some description about the book",
-  },
-  {
-    id: 2,
-    title: "Book 2",
-    author: "Sergey Petrov",
-    category: "Technical",
-    language: "English",
-    link: "https://www.amazon.com/C-Programming-Language-4th/dp/0321563840",
-    rating: 0,
-    status: "Not available",
-    description: "Some description about the book",
-  },
-  {
-    id: 3,
-    title: "Book 3",
-    author: "Petr Petrov",
-    category: "Technical",
-    language: "English",
-    link: "https://www.amazon.com/C-Programming-Language-4th/dp/0321563840",
-    rating: 0,
-    status: "Available",
-    description: "Some description about the book",
-  },
-];
+import books from "./books.json";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "GET") {
-    res.status(200).json(bookList);
+    res.status(200).json(books);
   }
   if (req.method === "POST") {
     let id;
-    if (!bookList.length) {
+    if (!books.length) {
       id = 1;
     } else {
-      id = bookList[bookList.length - 1].id + 1;
+      id = books[books.length - 1].id + 1;
     }
     const valuesOfBody = req.body;
 
@@ -61,7 +27,7 @@ export default function handler(req, res) {
       dateTo: valuesOfBody.dateTo,
     };
 
-    bookList.push(newBook);
+    books.push(newBook);
     return res.status(201).json(newBook);
   }
 }
