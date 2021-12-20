b7a6b8a3b4aef42acd6c07487aab6b6b2e241ba2
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-12-20 17:37:00 +0300

    fixed filter deleted cors annotation

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/filter/CORSFilter.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/filter/CORSFilter.java
@@ -15,19 +15,14 @@ public class CORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         chain.doFilter(req, res);
-
     }
+
 
     @Override
     public void init(FilterConfig filterConfig) {
