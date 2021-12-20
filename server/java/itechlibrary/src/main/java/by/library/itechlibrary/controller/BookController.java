6b7e9246238fc3c7f6304a2d82a5bc601a3d9848
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-12-20 17:17:50 +0300

    added filter

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/controller/BookController.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/controller/BookController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/books")
 @Api(tags = "Endpoints for books")
