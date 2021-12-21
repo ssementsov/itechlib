37eb5a12bd8c5271e05a6dd8510fec419c18fac8
LubavaZ <lubavazigalko@gmail.com>
2021-12-21 13:04:06 +0300

    add and delete book - not finished

--- src/pages/main-catalogue.js
+++ src/pages/main-catalogue.js
@@ -1,33 +1,45 @@
-import Head from "next/head";
-import { Box, Container, Typography } from "@mui/material";
-import { BooksListResults } from "../components/booksTable/books-list-results";
-import { BooksListToolbar } from "../components/booksTable/books-list-toolbar";
-import { DashboardLayout } from "../components/dashboard-layout";
-import { useState, useEffect } from "react";
-import { withSnackbar } from "notistack";
-import api from "../api/books";
+import Head from 'next/head'
+import { Box, Container, Typography } from '@mui/material'
+import { BooksListResults } from '../components/booksTable/books-list-results'
+import { BooksListToolbar } from '../components/booksTable/books-list-toolbar'
+import { DashboardLayout } from '../components/dashboard-layout'
+import { useState, useEffect } from 'react'
+import { withSnackbar } from 'notistack'
+import { status } from '../common/constants/status-constants'
+import api from '../api/books'
 
 const MainCatalogue = ({ enqueueSnackbar }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [books, setBooks] = useState();
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [books, setBooks] = useState()
   useEffect(() => {
     api
-      .get("/api/books")
+      .get('/api/books')
       .then(function (res) {
-        setBooks(res.data);
-        setIsLoaded(true);
+        setBooks(res.data)
+        setIsLoaded(true)
       })
       .catch(function () {
-        enqueueSnackbar("Something went wrong... Please retry.", {
-          variant: "error",
-        });
-      });
-  }, [enqueueSnackbar]);
+        enqueueSnackbar('Something went wrong... Please retry.', {
+          variant: 'error',
+        })
+      })
+  }, [enqueueSnackbar])
 
   const createBook = async (values) => {
-    let id = books.length ? books[books.length - 1].id + 1 : 1;
-    let idCategory = values.category === "Professional" ? 1 : 2;
-    let idLanguage = values.language === "English" ? 1 : 2;
+    let id = books.length ? books[books.length - 1].id + 1 : 1
+    let idCategory = values.category === 'Professional' ? 1 : 2
+    let idLanguage = values.language === 'English' ? 1 : 2
+    let idStatus
+    switch (values.status) {
+      case status.notAvailable:
+        idStatus = 2
+        break
+      case status.inUse:
+        idStatus = 3
+        break
+      default:
+        idStatus = 1
+    }
     const newBook = {
       author: values.author,
       title: values.title,
@@ -42,32 +54,36 @@ const MainCatalogue = ({ enqueueSnackbar }) => {
         id: idLanguage,
         name: values.language,
       },
-    };
+      status: {
+        id: idStatus,
+        name: values.status,
+      },
+    }
     await api
-      .post("/api/books", newBook)
+      .post('/api/books', newBook)
       .then(function (res) {
-        console.log(res);
-        enqueueSnackbar("Your book has been added successfully!", {
-          variant: "success",
-        });
-        const newBooksList = [newBook, ...books];
-        setBooks(newBooksList);
-        setIsLoaded(true);
+        console.log(res)
+        enqueueSnackbar('Your book has been added successfully!', {
+          variant: 'success',
+        })
+        const newBooksList = [newBook, ...books]
+        setBooks(newBooksList)
+        setIsLoaded(true)
       })
       .catch(function () {
-        enqueueSnackbar("Something went wrong... Please retry.", {
-          variant: "error",
-        });
-        setIsLoaded(true);
-      });
-  };
+        enqueueSnackbar('Something went wrong... Please retry.', {
+          variant: 'error',
+        })
+        setIsLoaded(true)
+      })
+  }
 
   if (!isLoaded) {
     return (
       <Typography sx={{ my: 8, mx: 4 }} variant="h4">
         Loading...
       </Typography>
-    );
+    )
   } else {
     return (
       <>
@@ -89,11 +105,11 @@ const MainCatalogue = ({ enqueueSnackbar }) => {
           </Container>
         </Box>
       </>
-    );
+    )
   }
-};
+}
 MainCatalogue.getLayout = (page) => {
-  return <DashboardLayout>{page}</DashboardLayout>;
-};
+  return <DashboardLayout>{page}</DashboardLayout>
+}
 
-export default withSnackbar(MainCatalogue);
+export default withSnackbar(MainCatalogue)
