4821adb93e6c8ba7aaa471abf79e9b8a408aca95
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-12-30 17:36:40 +0300

    added auth endpoint

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/controller/AuthController.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/controller/AuthController.java
@@ -3,10 +3,9 @@ package by.library.itechlibrary.controller;
 import by.library.itechlibrary.config.JwtProvider;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,9 +17,19 @@ public class AuthController {
 
 
     @PostMapping
-    public String auth(@RequestBody String corpEmail) {
+    public String getToken(@RequestBody String corpEmail) {
 
         return jwtProvider.generateToken(corpEmail);
+
+    }
+
+    @GetMapping
+    public String auth(@RequestBody Authentication authentication) {
+
+        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) authentication;
+        String email = authenticationToken.getPrincipal().getAttributes().get("email").toString();
+
+        return email;
 
     }
 }
