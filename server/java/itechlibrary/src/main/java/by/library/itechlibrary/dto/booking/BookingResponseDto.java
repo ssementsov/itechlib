862dcd5541968e5316c585a5a3da4f64cbdc492b
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-01-08 17:41:10 +0300

    added reader id.

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/booking/BookingResponseDto.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/booking/BookingResponseDto.java
@@ -20,4 +20,6 @@ public class BookingResponseDto {
     @NotNull
     private BookDto book;
 
+    private long readerId;
+
 }
--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/mapper/BookingMapper.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/mapper/BookingMapper.java
@@ -28,10 +28,11 @@ public interface BookingMapper {
     List<Booking> mapBookingListFromBookingDtoList(List<BookingDto> bookingDtos);
 
 
-
+    @Mapping(source = "reader.id", target = "readerId")
     @Named(value = "booking1")
     BookingResponseDto toNewBookingResponseDto(Booking booking);
 
+    @Mapping(source = "readerId", target = "reader.id")
     @Named(value = "newBookingResponseDto")
     Booking toBookingFromBookingResponseDto(BookingResponseDto bookingResponseDto);
 
