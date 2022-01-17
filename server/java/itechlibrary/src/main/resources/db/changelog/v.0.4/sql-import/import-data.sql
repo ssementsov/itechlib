aede053dec9cdc95988c56f237ada980516ca692
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-01-17 11:13:47 +0300

    added user data for example.

---
+++ server/java/itechlibrary/src/main/resources/db/changelog/v.0.4/sql-import/import-data.sql
@@ -0,0 +1,16 @@
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Maksim', 'Mayeusky', 'maksim.mayeuski@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Siarhei', 'Semiantsou', 'siarhei.semiantsou@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Yanina', 'Hutyro', 'yanina.hutyro@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Pavel', 'Baranau', 'Pavel.Baranau@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Liubou', 'Zhyhalka', 'liubou.zhyhalka@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Aleh', 'Bautunou', 'Aleh.Bautunou@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Ilyas', 'Sadziyeu', 'ilyas.sadziyeu@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
+INSERT INTO "user" (name, surname, corp_email, is_active)
+VALUES ('Aliaksandr', 'Silchanka', 'aliaksandr.silchanka@itechart-group.com', true) ON CONFLICT (corp_email) DO NOTHING;
