acfa5f51934f0a0d1f0b68562af21403715256e9
LubavaZ <lubavazigalko@gmail.com>
2022-01-25 11:05:42 +0300

    change port

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -14,139 +14,139 @@ import { GoogleLogout } from "react-google-login";
 import { LOGIN_PATH } from "../common/constants/route-constants";
 
 const DashboardNavbarRoot = styled(AppBar)(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-  boxShadow: theme.shadows[3],
+   backgroundColor: theme.palette.background.paper,
+   boxShadow: theme.shadows[3],
 }));
 
 const styleForMenu = {
-  elevation: 0,
-  sx: {
-    width: "150px",
-    overflow: "visible",
-    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-    mt: 1.5,
-    "& .MuiAvatar-root": {
-      width: 32,
-      height: 32,
-      ml: -0.5,
-      mr: 1,
-    },
-    "&:before": {
-      content: '""',
-      display: "block",
-      position: "absolute",
-      top: 0,
-      right: 14,
-      width: 10,
-      height: 10,
-      bgcolor: "background.paper",
-      transform: "translateY(-50%) rotate(45deg)",
-      zIndex: 0,
-    },
-  },
+   elevation: 0,
+   sx: {
+      width: "150px",
+      overflow: "visible",
+      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+      mt: 1.5,
+      "& .MuiAvatar-root": {
+         width: 32,
+         height: 32,
+         ml: -0.5,
+         mr: 1,
+      },
+      "&:before": {
+         content: '""',
+         display: "block",
+         position: "absolute",
+         top: 0,
+         right: 14,
+         width: 10,
+         height: 10,
+         bgcolor: "background.paper",
+         transform: "translateY(-50%) rotate(45deg)",
+         zIndex: 0,
+      },
+   },
 };
 
 export const DashboardNavbar = (props) => {
-  const router = useRouter();
-  const { onSidebarOpen, ...other } = props;
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const [avatar, setAvatar] = useState(null);
+   const router = useRouter();
+   const { onSidebarOpen, ...other } = props;
+   const [anchorEl, setAnchorEl] = React.useState(null);
+   const open = Boolean(anchorEl);
+   const [avatar, setAvatar] = useState(null);
 
-  const handleHover = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+   const handleHover = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    router.replace(LOGIN_PATH);
-  };
+   const handleLogout = () => {
+      localStorage.removeItem("token");
+      router.replace(LOGIN_PATH);
+   };
 
-  useEffect(() => {
-    setAvatar(localStorage.getItem("avatar"));
-  }, []);
+   useEffect(() => {
+      setAvatar(localStorage.getItem("avatar"));
+   }, []);
 
-  return (
-    <>
-      <DashboardNavbarRoot
-        sx={{
-          left: {
-            lg: 280,
-          },
-          width: {
-            lg: "calc(100% - 280px)",
-          },
-        }}
-        {...other}
-      >
-        <Toolbar
-          disableGutters
-          sx={{
-            minHeight: 64,
-            left: 0,
-            px: 2,
-          }}
-        >
-          <IconButton
-            onClick={onSidebarOpen}
+   return (
+      <>
+         <DashboardNavbarRoot
             sx={{
-              display: {
-                xs: "inline-flex",
-                lg: "none",
-              },
+               left: {
+                  lg: 280,
+               },
+               width: {
+                  lg: "calc(100% - 280px)",
+               },
             }}
-          >
-            <MenuIcon fontSize="small" />
-          </IconButton>
-          <Box sx={{ flexGrow: 1 }} />
-          <IconButton onClick={handleHover} size="small" sx={{ ml: 2 }}>
-            <Avatar
-              src={avatar}
-              sx={{
-                height: 40,
-                width: 40,
-                ml: 1,
-              }}
-            ></Avatar>
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            onClick={handleClose}
-            PaperProps={{ ...styleForMenu }}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          >
-            <MenuItem>
-              <Avatar src={avatar} /> Profile
-            </MenuItem>
-            <Divider />
-            <GoogleLogout
-              clientId={process.env.GOOGLE_CLIENT_ID}
-              buttonText="Logout"
-              isSignedIn={false}
-              onLogoutSuccess={handleLogout}
-              onFailure={handleLogout}
-              render={(renderProps) => (
-                <MenuItem onClick={renderProps.onClick}>
-                  <ListItemIcon>
-                    <Logout fontSize="small" />
-                  </ListItemIcon>
-                  Logout
-                </MenuItem>
-              )}
-            ></GoogleLogout>
-          </Menu>
-        </Toolbar>
-      </DashboardNavbarRoot>
-    </>
-  );
+            {...other}
+         >
+            <Toolbar
+               disableGutters
+               sx={{
+                  minHeight: 64,
+                  left: 0,
+                  px: 2,
+               }}
+            >
+               <IconButton
+                  onClick={onSidebarOpen}
+                  sx={{
+                     display: {
+                        xs: "inline-flex",
+                        lg: "none",
+                     },
+                  }}
+               >
+                  <MenuIcon fontSize="small" />
+               </IconButton>
+               <Box sx={{ flexGrow: 1 }} />
+               <IconButton onClick={handleHover} size="small" sx={{ ml: 2 }}>
+                  <Avatar
+                     src={avatar}
+                     sx={{
+                        height: 40,
+                        width: 40,
+                        ml: 1,
+                     }}
+                  ></Avatar>
+               </IconButton>
+               <Menu
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{ ...styleForMenu }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+               >
+                  <MenuItem>
+                     <Avatar src={avatar} /> Profile
+                  </MenuItem>
+                  <Divider />
+                  <GoogleLogout
+                     clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+                     buttonText="Logout"
+                     isSignedIn={false}
+                     onLogoutSuccess={handleLogout}
+                     onFailure={handleLogout}
+                     render={(renderProps) => (
+                        <MenuItem onClick={renderProps.onClick}>
+                           <ListItemIcon>
+                              <Logout fontSize="small" />
+                           </ListItemIcon>
+                           Logout
+                        </MenuItem>
+                     )}
+                  ></GoogleLogout>
+               </Menu>
+            </Toolbar>
+         </DashboardNavbarRoot>
+      </>
+   );
 };
 
 DashboardNavbar.propTypes = {
-  onSidebarOpen: PropTypes.func,
+   onSidebarOpen: PropTypes.func,
 };
