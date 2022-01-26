414aaec76dd0860b347c0daf878a51f2a4fce33e
LubavaZ <81172035+LubavaZ@users.noreply.github.com>
2022-01-26 14:35:38 +0300

    LIB-74 delete book cover (#70)

--- src/components/book/book-cover-image/hidden-book-cover-tools.js
+++ src/components/book/book-cover-image/hidden-book-cover-tools.js
@@ -1,45 +1,50 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Button, IconButton } from "@mui/material";
-import { LightDeleteIcon } from "./../../../icons/light-delete-icon";
-import StyledCard from "./styled-card";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, IconButton } from '@mui/material';
+import { LightDeleteIcon } from './../../../icons/light-delete-icon';
+import StyledCard from './styled-card';
 
 const HiddenBookCoverTools = (props) => {
-   const { onUploadButtonOpen, visible, isUploaded } = props;
+    const { onUploadButtonOpen, visible, isUploaded, onOpen } = props;
 
-   return (
-      <StyledCard
-         sx={{
-            display: visible && isUploaded ? "block" : "none",
-            background: "rgba(0, 0, 0, 0.6)",
-         }}
-      >
-         <div
-            style={{
-               position: "relative",
-               display: "flex",
-               justifyContent: "center",
-               height: "100%",
+    return (
+        <StyledCard
+            sx={{
+                display: visible && isUploaded ? 'block' : 'none',
+                background: 'rgba(0, 0, 0, 0.6)',
             }}
-         >
-            <IconButton
-               aria-label="delete"
-               sx={{ position: "absolute", top: 0, right: 0 }}
+        >
+            <div
+                style={{
+                    position: 'relative',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    height: '100%',
+                }}
             >
-               <LightDeleteIcon fontSize="larg" />
-            </IconButton>
-            <Button onClick={onUploadButtonOpen} sx={{ mr: 1, color: "white" }}>
-               Upload image
-            </Button>
-         </div>
-      </StyledCard>
-   );
+                <IconButton
+                    onClick={onOpen}
+                    aria-label="delete"
+                    sx={{ position: 'absolute', top: 0, right: 0 }}
+                >
+                    <LightDeleteIcon fontSize="larg" />
+                </IconButton>
+                <Button
+                    onClick={onUploadButtonOpen}
+                    sx={{ mr: 1, color: 'white' }}
+                >
+                    Upload image
+                </Button>
+            </div>
+        </StyledCard>
+    );
 };
 
 HiddenBookCoverTools.propTypes = {
-   onUploadButtonOpen: PropTypes.func.isRequired,
-   visible: PropTypes.bool.isRequired,
-   isUploaded: PropTypes.bool.isRequired,
+    onUploadButtonOpen: PropTypes.func.isRequired,
+    visible: PropTypes.bool.isRequired,
+    isUploaded: PropTypes.bool.isRequired,
+    onOpen: PropTypes.func.isRequired,
 };
 
 export default HiddenBookCoverTools;
