d5a5ad354a8b8e23fedb5d95295c4315dc508abf
LubavaZ <lubavazigalko@gmail.com>
2022-01-27 17:49:31 +0300

    change variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 class Api {
-  constructor() {
-    let client = axios.create();
-    const BASE_URL = 'http://localhost:8089/api';
-    client.defaults.baseURL = BASE_URL;
-    this.Client = client;
-  }
+    constructor() {
+        let client = axios.create();
+        const BASE_URL = process.env.NEXT_PUBLIC_BOOKS_ENDPOINT;
+        client.defaults.baseURL = BASE_URL;
+        this.Client = client;
+    }
 
-  setupAuth(token) {
-    this.Client.defaults.headers = {
-      Authorization: `Bearer ${token}`,
-    };
-  }
+    setupAuth(token) {
+        this.Client.defaults.headers = {
+            Authorization: `Bearer ${token}`,
+        };
+    }
 }
 
 export const api = new Api();
