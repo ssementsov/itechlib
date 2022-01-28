efb4b2d7a4028e5b9efccdbb7c363fa7fa4d658f
LubavaZ <lubavazigalko@gmail.com>
2022-01-28 12:34:21 +0300

    added loader

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { Box, Container, Grid, Button } from '@mui/material';
+import { Box, Container, Grid, Button, Typography } from '@mui/material';
 import { UserAPI } from '../api/user-api';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { DashboardLayout } from '../components/dashboard-layout';
@@ -12,6 +12,7 @@ import ProfileDetails from './../components/profile/profile-details';
 function ProfilePage() {
     const router = useRouter();
     const [user, setUser] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
     const [isOwner, setIsOwner] = useState(false);
     const [isUploadedAvatar, setIsUploadedAvatar] = useState(false);
     const [isUpdatedAvatar, setIsUpdatedAvatar] = useState(false);
@@ -42,6 +43,7 @@ function ProfilePage() {
         UserAPI.getUser()
             .then((res) => {
                 setUser(res.data);
+                setIsLoaded(true);
                 setIsOwner(true);
                 let avatar = res.data.fileInfo;
                 if (avatar) {
@@ -51,56 +53,64 @@ function ProfilePage() {
             .catch(() => defaultErrorSnackbar());
     }, [defaultErrorSnackbar, isUpdatedAvatar, isUploadedAvatar]);
 
-    return (
-        <>
-            <Head>
-                <title>Profile page</title>
-            </Head>
-            <Box
-                component="main"
-                sx={{
-                    flexGrow: 1,
-                    pt: 3,
-                    pb: 8,
-                }}
-            >
-                <Button
-                    onClick={() => router.back()}
-                    component="a"
-                    startIcon={<ArrowBackIcon fontSize="small" />}
+    if (!isLoaded) {
+        return (
+            <Typography sx={{ my: 8, mx: 4 }} variant="h4">
+                Loading...
+            </Typography>
+        );
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>Profile page</title>
+                </Head>
+                <Box
+                    component="main"
                     sx={{
-                        ml: 2,
+                        flexGrow: 1,
+                        pt: 3,
+                        pb: 8,
                     }}
                 >
-                    Back
-                </Button>
-                <Container
-                    maxWidth="lg"
-                    sx={{
-                        pt: 11,
-                    }}
-                >
-                    <Grid container spacing={12}>
-                        <Grid item lg={4} md={4} xs={12}>
-                            <UploadImageCard
-                                isUploaded={isUploadedAvatar}
-                                onUpdate={setIsUpdatedAvatar}
-                                onUpload={setIsUploadedAvatar}
-                                data={user}
-                                isOwner={isOwner}
-                                onAdd={addAvatar}
-                                onDelete={deleteAvatar}
-                                title={'avatar'}
-                            />
+                    <Button
+                        onClick={() => router.back()}
+                        component="a"
+                        startIcon={<ArrowBackIcon fontSize="small" />}
+                        sx={{
+                            ml: 2,
+                        }}
+                    >
+                        Back
+                    </Button>
+                    <Container
+                        maxWidth="lg"
+                        sx={{
+                            pt: 11,
+                        }}
+                    >
+                        <Grid container spacing={12}>
+                            <Grid item lg={4} md={4} xs={12}>
+                                <UploadImageCard
+                                    isUploaded={isUploadedAvatar}
+                                    onUpdate={setIsUpdatedAvatar}
+                                    onUpload={setIsUploadedAvatar}
+                                    data={user}
+                                    isOwner={isOwner}
+                                    onAdd={addAvatar}
+                                    onDelete={deleteAvatar}
+                                    title={'avatar'}
+                                />
+                            </Grid>
+                            <Grid item lg={8} md={8} xs={12}>
+                                <ProfileDetails user={user} />
+                            </Grid>
                         </Grid>
-                        <Grid item lg={8} md={8} xs={12}>
-                            <ProfileDetails user={user} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Box>
-        </>
-    );
+                    </Container>
+                </Box>
+            </>
+        );
+    }
 }
 
 ProfilePage.getLayout = (page) => {
