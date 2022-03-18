8368e362ce4378a02354d269b1d6541b26ff63ef
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-03-18 17:37:40 +0400

    added constructors

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/book/FullBookDto.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/book/FullBookDto.java
@@ -1,9 +1,13 @@
 package by.library.itechlibrary.dto.book;
 
 import by.library.itechlibrary.dto.FileInfoDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FullBookDto extends WithOwnerBookDto {
 
     private boolean isReader;
--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/book/WithOwnerBookDto.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/book/WithOwnerBookDto.java
@@ -1,9 +1,13 @@
 package by.library.itechlibrary.dto.book;
 
 import by.library.itechlibrary.dto.UserDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WithOwnerBookDto extends BookDto {
 
     private UserDto owner;
