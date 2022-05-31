4fa0150d432905b24674febf475292de5b7b34f9
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-05-31 11:38:08 +0400

    added new constants

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/constant/MailTemplateConstant.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/constant/MailTemplateConstant.java
@@ -9,6 +9,11 @@ public final class MailTemplateConstant {
     public static final String HOST = "{HOST}";
     public static final String USER_ID = "{USER_ID}";
     public static final String CONFIRMATION_CODE = "{CONFIRMATION_CODE}";
+    public static final String BOOK_TITLE = "{BOOK_TITLE}";
+    public static final String BOOK_READER_NAME = "{BOOK_READER_NAME}";
+    public static final String BOOKING_START_DATE = "{BOOKING_START_DATE}";
+    public static final String BOOKING_END_DATE = "{BOOKING_END_DATE}";
+    public static final String OWNER_NAME = "{OWNER_NAME}";
 
     public static final String MAIL_CONFIRMATION = "MAIL_CONFIRMATION_TEMPLATE";
 
