06d1a645c223eb8542d274a89c0dc86b8c0a804b
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-07-12 13:57:20 +0400

    comment tests and add roles to UserDto

--- server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/UserDto.java
+++ server/java/itechlibrary/src/main/java/by/library/itechlibrary/dto/UserDto.java
@@ -1,11 +1,13 @@
 package by.library.itechlibrary.dto;
 
 import by.library.itechlibrary.constant.RegexConstant;
+import by.library.itechlibrary.entity.UserRole;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Data
 public class UserDto {
@@ -26,5 +28,7 @@ public class UserDto {
     @Email(regexp = RegexConstant.USER_GOOGLE_EMAIL_REGEX, message = "Wrong google mail")
     @Size(min = 24, max = 50)
     private String googleEmail;
+
+    private Set<UserRoleDto> roles;
 
 }
