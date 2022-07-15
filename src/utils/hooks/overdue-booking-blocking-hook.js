b83213be64f5e83285e7e9209718a815075c0474
LubavaZ <lubavazigalko@gmail.com>
2022-07-15 14:54:02 +0300

    fix

--- src/utils/hooks/overdue-booking-blocking-hook.js
+++ src/utils/hooks/overdue-booking-blocking-hook.js
@@ -4,9 +4,9 @@ import { useSelector } from 'react-redux';
 export const useOverdueBookingBlocking = () => {
     const [isBlockingModalOpen, setBlockingModalOpen, setBlockingModalClose] = useBoolean();
     const roles = useSelector(state => state.user.user.roles);
-    const isReaderRole = roles.length > 0;
+    const isReaderRole = roles?.length > 0;
 
-    const handleBlockingOrAction = (e, handleAction) => {
+    const handleBlockingOrAction = (handleAction, e) => {
         isReaderRole
             ? handleAction()
             : setBlockingModalOpen(e);
