220e35896583885dd0288986f687b33d4e9274a9
LubavaZ <lubavazigalko@gmail.com>
2022-08-29 19:22:25 +0300

    delete perfect scrollbar

--- src/components/books-in-use/books-in-use-list-results.js
+++ src/components/books-in-use/books-in-use-list-results.js
@@ -1,4 +1,3 @@
-import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import {
@@ -26,79 +25,77 @@ const StyledTableCell = styled(TableCell)(() => ({ width: 115, textAlign: 'cente
 const BooksInUseListResults = ({ books, isStartedSearch }) => {
     return (
         <Card>
-            <PerfectScrollbar>
-                <Box sx={{ minWidth: 1050 }}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>{titles.title}</TableCell>
-                                <TableCell>{titles.author}</TableCell>
-                                <TableCell>{titles.description}</TableCell>
-                                <StyledTableCell>{titles.category}</StyledTableCell>
-                                <StyledTableCell>{titles.language}</StyledTableCell>
-                                <StyledTableCell>{titles.rate}</StyledTableCell>
-                                <StyledTableCell>{titles.dueDate}</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {books.length ? (
-                                books.map((book) => {
-                                    return (
-                                        <TableRow
-                                            onClick={() =>
-                                                router.push(`${BOOK_PREVIEW_PAGE_PATH}/${book.id}`)
-                                            }
-                                            key={book.id}
-                                            hover
-                                        >
-                                            <TableCell>{trimmedString(book.title)}</TableCell>
-                                            <TableCell>{trimmedString(book.author)}</TableCell>
-                                            <TableCell>{trimmedString(book.description)}</TableCell>
-                                            <StyledTableCell>
-                                                {toLowerCaseExceptFirstLetter(book.category.name)}
-                                            </StyledTableCell>
-                                            <StyledTableCell>
-                                                {toLowerCaseExceptFirstLetter(book.language.name)}
-                                            </StyledTableCell>
-                                            <StyledTableCell>
-                                                <Tooltip title={book.rate} placement="right">
+            <Box sx={{ minWidth: 1050 }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>{titles.title}</TableCell>
+                            <TableCell>{titles.author}</TableCell>
+                            <TableCell>{titles.description}</TableCell>
+                            <StyledTableCell>{titles.category}</StyledTableCell>
+                            <StyledTableCell>{titles.language}</StyledTableCell>
+                            <StyledTableCell>{titles.rate}</StyledTableCell>
+                            <StyledTableCell>{titles.dueDate}</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {books.length ? (
+                            books.map((book) => {
+                                return (
+                                    <TableRow
+                                        onClick={() =>
+                                            router.push(`${BOOK_PREVIEW_PAGE_PATH}/${book.id}`)
+                                        }
+                                        key={book.id}
+                                        hover
+                                    >
+                                        <TableCell>{trimmedString(book.title)}</TableCell>
+                                        <TableCell>{trimmedString(book.author)}</TableCell>
+                                        <TableCell>{trimmedString(book.description)}</TableCell>
+                                        <StyledTableCell>
+                                            {toLowerCaseExceptFirstLetter(book.category.name)}
+                                        </StyledTableCell>
+                                        <StyledTableCell>
+                                            {toLowerCaseExceptFirstLetter(book.language.name)}
+                                        </StyledTableCell>
+                                        <StyledTableCell>
+                                            <Tooltip title={book.rate} placement='right'>
                                                     <span>
                                                         <Rating
                                                             precision={0.5}
-                                                            name="read-only"
+                                                            name='read-only'
                                                             value={calculateRate(book.rate)}
-                                                            size="small"
+                                                            size='small'
                                                             readOnly
                                                         />
                                                     </span>
-                                                </Tooltip>
-                                            </StyledTableCell>
-                                            <StyledTableCell>
-                                                {getFormatedDate(book.baseBookingInfo.bookingEndDate)}
-                                            </StyledTableCell>
-                                        </TableRow>
-                                    );
-                                })
-                            ) : (
-                                <TableRow>
-                                    <TableCell colSpan={8}>
-                                        <Typography
-                                            sx={{
-                                                textAlign: 'center',
-                                                color: 'action.active',
-                                            }}
-                                        >
-                                            {isStartedSearch
-                                                ? 'No books found'
-                                                : 'No books assigned to you yet!'}
-                                        </Typography>
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </Box>
-            </PerfectScrollbar>
+                                            </Tooltip>
+                                        </StyledTableCell>
+                                        <StyledTableCell>
+                                            {getFormatedDate(book.baseBookingInfo.bookingEndDate)}
+                                        </StyledTableCell>
+                                    </TableRow>
+                                );
+                            })
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={8}>
+                                    <Typography
+                                        sx={{
+                                            textAlign: 'center',
+                                            color: 'action.active',
+                                        }}
+                                    >
+                                        {isStartedSearch
+                                            ? 'No books found'
+                                            : 'No books assigned to you yet!'}
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </Box>
         </Card>
     );
 };
