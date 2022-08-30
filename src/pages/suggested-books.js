dde2a69b402838476bfec7fc938cafcb464dcd50
LubavaZ <lubavazigalko@gmail.com>
2022-08-30 09:52:58 +0300

    fixed

--- src/pages/suggested-books.js
+++ src/pages/suggested-books.js
@@ -1,69 +1,72 @@
-
-import {useEffect, useState} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { DashboardLayout } from '../components/dashboard-layout';
 import BooksCatalogue from '../components/books-catalogue';
-import { SuggestionAPI } from './../api/suggested-books-api';
+import { SuggestionAPI } from '../api/suggested-books-api';
 import { useInfiniteScroll } from '../utils/hooks/infinite-scroll-hook';
 import { ProgressLinear } from '../common/UI/progressLinear';
-import {SortFields, SortDirection} from '../common/constants/sorting-constants';
+import { SortDirection, SortFields } from '../common/constants/sorting-constants';
 
 const SuggestedBooksCatalogue = () => {
     const [suggestedBooks, setSuggestedBooks] = useState([]);
     const [filters, setFilters] = useState([]);
-    const [sortings, setSortings] = useState({sortDirection: SortDirection.desc, sortField: SortFields.createDate})
+    const [sortings, setSortings] = useState({ sortDirection: SortDirection.desc, sortField: SortFields.createDate });
     const requestApi = (currentPage) => SuggestionAPI.getSuggestedBooksList(filters, sortings, currentPage);
     const {
         isLoaded,
         setIsLoaded,
         setEmptyPage,
-        setCurrentPage
+        setCurrentPage,
     } = useInfiniteScroll(requestApi, suggestedBooks, setSuggestedBooks);
+
+    const getSuggestedBooksListHandler = useCallback((data) => {
+        setEmptyPage(false);
+        setCurrentPage(1);
+        setSuggestedBooks([...data]);
+    }, [setCurrentPage, setEmptyPage]);
 
     useEffect(() => {
         SuggestionAPI.getSuggestedBooksList(filters, sortings)
             .then(res => {
-                setEmptyPage(false);
-                setCurrentPage(1);
-                setSuggestedBooks([...res.data]);
-            })
-    }, [filters, sortings])
+                getSuggestedBooksListHandler(res.data);
+            });
+    }, [filters, getSuggestedBooksListHandler, sortings]);
 
     const generateFilters = (value, name) => {
         const newFilter = {
             field: `${name}.name`,
-            value: value
-        }
+            value: value,
+        };
         setFilters(prevFilterList => {
-            if(prevFilterList.length === 0) {
-                if(!value) {
+            if (prevFilterList.length === 0) {
+                if (!value) {
                     return prevFilterList;
                 } else {
                     return [...prevFilterList, newFilter];
                 }
             } else {
                 const isExistedFilterType = prevFilterList.some(filter => filter.field === `${name}.name`);
-                if(isExistedFilterType) {
+                if (isExistedFilterType) {
                     const newFilterList = prevFilterList.map(filter => {
-                        if(filter.field === `${name}.name`) {
+                        if (filter.field === `${name}.name`) {
                             return {
                                 ...filter,
-                                value: value
-                            }
+                                value: value,
+                            };
                         } else {
-                            return filter
+                            return filter;
                         }
-                    }).filter(filter => filter.value !== '')
-                    return newFilterList
+                    }).filter(filter => filter.value !== '');
+                    return newFilterList;
                 } else {
-                    if(!value) {
+                    if (!value) {
                         return prevFilterList;
                     } else {
                         return [...prevFilterList, newFilter];
                     }
                 }
             }
-        })
-    }
+        });
+    };
 
     if (!isLoaded) {
         return <ProgressLinear />;
